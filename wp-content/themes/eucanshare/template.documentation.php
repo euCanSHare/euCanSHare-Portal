14b8f948f777074c00e5119d25cf2203f1e0980e
acavalls <alejandro.canosa@bsc.es>
2020-03-19 09:25:47 +0000

    Documentation: Adding 'Join the platform' section

--- wp-content/themes/eucanshare/template.documentation.php
+++ wp-content/themes/eucanshare/template.documentation.php
@@ -9,12 +9,8 @@ Template Name: Docs
 
 <?php get_header(); ?>
 
-<section id="" class="page-default single">
-	<div class="item-img cover cover-center primary-image" style="height:100px;background-image:url(<?php echo  the_post_thumbnail_url('large');  ?>)"></div>
-        <div class="content-overlap">
-            <div class="wrapper ">
-                <div class="container">
-                    <h2 class="section-title"><?php the_title();?></h2>
+    <div class="container">
+        <h2 class="section-title"><?php the_title();?></h2>
 
         <div class="row inner-box-col">
             <div class="col-lg-12">
@@ -33,14 +29,28 @@ Template Name: Docs
                 <p><a class="inner-box-link" href="/dataportal/?page_id=697"> 3) Integration of tools </a></p>
             </div>
         </div>
+        <div class="row inner-box-col">
+            <div class="col-lg-12">
+                <p class="inner-box-title"> Join the platform </p>
+            </div>
+
+            <div class="col-lg-12">
+                <p><a class="inner-box-link" href="/dataportal/?page_id=1406"> 1) Registration </a></p>
+            </div>
+
+            <div class="col-lg-12">
+                <p><a class="inner-box-link" href="/dataportal/?page_id=1383"> 2) Login </a></p>
+            </div>
+
+            <div class="col-lg-12">
+                <p><a class="inner-box-link" href="/dataportal/?page_id=1426"> 3) Request administrative privileges </a></p>
+            </div>
+
+        </div>
         <br>
 
         <?php get_template_part('includes/section', "content"); ?>
 
-        </div>
-                </div>
-            </div>
-        </div>
-</section>
+    </div>
 
 <?php get_footer(); ?>
